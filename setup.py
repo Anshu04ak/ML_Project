0bcd780aa58a9ac13b1ff60e201db14e95a1d63d
Anshu Kumari <anshu.ak238@gmail.com>
2024-03-28 15:28:13 +0530

    Update setup.py and remove trials.ipynb

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
 import setuptools
+from typing import list
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
+# with open("README.md", "r", encoding="utf-8") as f:
+#     long_description = f.read()
+
+Hypen_e_dot="-e ."
+
+def get_requirwments(file_path:str)->list[str]:
+    '''
+    This function will return list of the requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n"," ") for req in requirements]
+
+        if Hypen_e_dot in requirements:
+            requirements.remove(Hypen_e_dot)
+    
+    return requirements
+
 
 
 __version__ = "0.0.0"
@@ -18,12 +36,13 @@ setuptools.setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A small python package for ml app",
-    long_description=long_description,
-    long_description_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-    },
-    package_dir={"": "src"},
+    # long_description=long_description,
+    # long_description_content="text/markdown",
+    # url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    # project_urls={
+    #     "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    # },
+    # package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
+    install_requires=get_requirements('requirements.txt')
 )
